1914f7eca8e778e1874e1ee34621734a288b161c
Elizaveta Zabrodskaya <e_zabrodskaya@lesta.group>
2023-02-28 16:21:09 +0300

    3 task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+	return str
+		.split('-')
+		.map((elem, i) => {
+			if (i !== 0) {
+				elem = elem[0].toUpperCase() + elem.slice(1);
+			}
+			return elem;
+		})
+		.join('');
 }
