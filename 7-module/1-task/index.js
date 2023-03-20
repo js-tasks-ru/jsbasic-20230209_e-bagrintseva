44de191fe9668381a6662a1800351b77f6a98ca6
Elizaveta Zabrodskaya <e_zabrodskaya@lesta.group>
2023-03-20 15:58:20 +0300

    7 module task 1

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,110 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class RibbonMenu {
-  constructor(categories) {
-    this.categories = categories;
-  }
+	constructor(categories) {
+		this.categories = categories;
+
+		this.render();
+		this.scroll();
+		this.chooseItem();
+	}
+
+	render() {
+		let list = this.categories.map((category) => {
+			return `<a href='#' class='ribbon__item' data-id='${category.id}'>${category.name}</a>`;
+		});
+
+		this.elem = createElement(`
+    <div class="ribbon">
+      <button class="ribbon__arrow ribbon__arrow_left">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+
+      <nav class="ribbon__inner">
+        ${list.join('')}
+      </nav>
+
+      <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    </div>
+    `);
+
+		this.ribbonInner = this.elem.querySelector('.ribbon__inner');
+
+		return this.elem;
+	}
+
+	scroll = () => {
+		this.elem.addEventListener('click', (evt) => {
+			if (evt.target.closest('.ribbon__arrow_right')) {
+				this.next();
+				this.ribbonInner.addEventListener('scroll', () => {
+					this.update();
+				});
+			}
+			if (evt.target.closest('.ribbon__arrow_left')) {
+				this.prev();
+				this.ribbonInner.addEventListener('scroll', () => {
+					this.update();
+				});
+			}
+		});
+	};
+
+	chooseItem = () => {
+		let links = this.elem.querySelectorAll('.ribbon__item');
+
+		this.elem.addEventListener('click', (evt) => {
+			if (evt.target.closest('.ribbon__item')) {
+				evt.preventDefault();
+				let item = evt.target.closest('.ribbon__item');
+				links.forEach((link) => {
+					link.classList.remove('ribbon__item_active');
+				});
+				item.classList.add('ribbon__item_active');
+				let id = item.dataset.id;
+
+				this.elem.dispatchEvent(
+					new CustomEvent('ribbon-select', {
+						detail: id,
+						bubbles: true,
+					})
+				);
+			}
+		});
+	};
+
+	next() {
+		this.ribbonInner.scrollBy(350, 0);
+	}
+
+	prev() {
+		this.ribbonInner.scrollBy(-350, 0);
+	}
+
+	update() {
+		let arrowLeft = this.elem.querySelector('.ribbon__arrow_left');
+		let arrowRight = this.elem.querySelector('.ribbon__arrow_right');
+
+		let scrollWidth = this.ribbonInner.scrollWidth;
+		let scrollLeft = this.ribbonInner.scrollLeft;
+		let clientWidth = this.ribbonInner.clientWidth;
+
+		let scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+		arrowLeft.classList.add('ribbon__arrow_visible');
+		arrowRight.classList.add('ribbon__arrow_visible');
+
+		if (scrollLeft === 0) {
+			arrowLeft.classList.remove('ribbon__arrow_visible');
+		} else {
+			arrowLeft.classList.add('ribbon__arrow_visible');
+		}
+		if (scrollRight < 1) {
+			arrowRight.classList.remove('ribbon__arrow_visible');
+		} else {
+			arrowRight.classList.add('ribbon__arrow_visible');
+		}
+	}
 }
--- 7-module/1-task/task.test.js
+++ 7-module/1-task/task.test.js
@@ -2,96 +2,99 @@ import categories from './categories.js';
 import RibbonMenu from './index.js';
 
 describe('7-module-1-task', () => {
-  let sut;
+	let sut;
 
-  let ribbonInner;
-  let ribbonArrowRight;
-  let ribbonArrowLeft;
+	let ribbonInner;
+	let ribbonArrowRight;
+	let ribbonArrowLeft;
 
-  let clickEvent;
+	let clickEvent;
 
-  beforeEach(() => {
-    sut = new RibbonMenu(categories);
-    document.body.append(sut.elem);
+	beforeEach(() => {
+		sut = new RibbonMenu(categories);
+		document.body.append(sut.elem);
 
-    ribbonInner = sut.elem.querySelector('.ribbon__inner');
+		ribbonInner = sut.elem.querySelector('.ribbon__inner');
 
-    ribbonArrowRight = sut.elem.querySelector('.ribbon__arrow_right');
-    ribbonArrowLeft = sut.elem.querySelector('.ribbon__arrow_left');
+		ribbonArrowRight = sut.elem.querySelector('.ribbon__arrow_right');
+		ribbonArrowLeft = sut.elem.querySelector('.ribbon__arrow_left');
 
-    clickEvent = new MouseEvent('click', { bubbles: true });
-  });
+		clickEvent = new MouseEvent('click', {bubbles: true});
+	});
 
-  afterEach(() => {
-    sut.elem.remove();
-  });
+	afterEach(() => {
+		sut.elem.remove();
+	});
 
-  describe('отрисовка вёрстки после создания', () => {
-    it('должна добавлять корневой элемент в свойство "elem"', () => {
-      expect(sut.elem.classList.contains('ribbon')).toBe(true);
-    });
+	describe('отрисовка вёрстки после создания', () => {
+		it('должна добавлять корневой элемент в свойство "elem"', () => {
+			expect(sut.elem.classList.contains('ribbon')).toBe(true);
+		});
 
-    it('должна отрисовать все категории', () => {
-      let categoriesElements = sut.elem.querySelectorAll('.ribbon__item');
+		it('должна отрисовать все категории', () => {
+			let categoriesElements = sut.elem.querySelectorAll('.ribbon__item');
 
-      expect(categoriesElements.length).toBe(9);
-    });
-  });
+			expect(categoriesElements.length).toBe(9);
+		});
+	});
 
-  describe('прокрутка', () => {
-    let scrollBySpy;
+	describe('прокрутка', () => {
+		let scrollBySpy;
 
-    beforeEach(() => {
-      scrollBySpy = spyOn(ribbonInner, 'scrollBy').and.callThrough();
-    });
+		beforeEach(() => {
+			scrollBySpy = spyOn(ribbonInner, 'scrollBy').and.callThrough();
+		});
 
-    describe('вперёд', () => {
-      it('при клике на кнопке "вперёд", должна прокрутить на 350px вперёд', () => {
-        ribbonArrowRight.dispatchEvent(clickEvent);
+		describe('вперёд', () => {
+			it('при клике на кнопке "вперёд", должна прокрутить на 350px вперёд', () => {
+				ribbonArrowRight.dispatchEvent(clickEvent);
 
-        expect(scrollBySpy).toHaveBeenCalledWith(350, 0);
-      });
-    });
+				expect(scrollBySpy).toHaveBeenCalledWith(350, 0);
+			});
+		});
 
-    describe('назад', () => {
-      beforeEach(() => {
-        ribbonArrowRight.dispatchEvent(clickEvent);
-        ribbonArrowRight.dispatchEvent(clickEvent);
-      });
+		describe('назад', () => {
+			beforeEach(() => {
+				ribbonArrowRight.dispatchEvent(clickEvent);
+				ribbonArrowRight.dispatchEvent(clickEvent);
+			});
 
-      it('при клике по кнопке "назад", должна прокрутить на 350px назад', () => {
-        ribbonArrowLeft.dispatchEvent(clickEvent);
+			it('при клике по кнопке "назад", должна прокрутить на 350px назад', () => {
+				ribbonArrowLeft.dispatchEvent(clickEvent);
 
-        expect(scrollBySpy).toHaveBeenCalledWith(-350, 0);
-      });
-    });
+				expect(scrollBySpy).toHaveBeenCalledWith(-350, 0);
+			});
+		});
+	});
 
-  });
+	describe('выбор категории', () => {
+		let ribbonSelectEventName;
+		let ribbonSelectEvent;
+		let category;
 
-  describe('выбор категории', () => {
-    let ribbonSelectEventName;
-    let ribbonSelectEvent;
-    let category;
+		beforeEach(() => {
+			ribbonSelectEventName = 'ribbon-select';
 
-    beforeEach(() => {
-      ribbonSelectEventName = 'ribbon-select';
+			document.body.addEventListener(
+				ribbonSelectEventName,
+				(event) => {
+					ribbonSelectEvent = event;
+				},
+				{once: true}
+			);
 
-      document.body.addEventListener(ribbonSelectEventName, (event) => {
-        ribbonSelectEvent = event;
-      }, { once: true });
+			category = categories[1];
+			let categorySelectButton = sut.elem.querySelector(`[data-id="${category.id}"]`);
 
-      category = categories[1];
-      let categorySelectButton = sut.elem.querySelector(`[data-id="${category.id}"]`);
+			categorySelectButton.dispatchEvent(clickEvent);
+		});
 
-      categorySelectButton.dispatchEvent(clickEvent);
-    });
+		it('после клика по ссылке, должно быть создано событие', () => {
+			expect(ribbonSelectEvent instanceof CustomEvent).toBe(true);
+		});
 
-    it('после клика по ссылке, должно быть создано событие', () => {
-      expect(ribbonSelectEvent instanceof CustomEvent).toBe(true);
-    });
-
-    it('созданное событие должно содержать в себе уникальный идентификатор товара ("id")', () => {
-      expect(ribbonSelectEvent.detail).toBe(category.id);
-    });
-  });
+		it('созданное событие должно содержать в себе уникальный идентификатор товара ("id")', () => {
+			expect(ribbonSelectEvent.detail).toBe(category.id);
+		});
+	});
 });
