4b9195cf47bbcbadd5663f56f6eace477924fee6
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2023-03-26 13:52:51 +0300

    8 module

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -1,34 +1,80 @@
 export default class Cart {
-  cartItems = []; // [product: {...}, count: N]
+	cartItems = []; // [product: {...}, count: N]
 
-  constructor(cartIcon) {
-    this.cartIcon = cartIcon;
-  }
+	constructor(cartIcon) {
+		this.cartIcon = cartIcon;
+	}
 
-  addProduct(product) {
-    // ваш код
-  }
+	addProduct(product) {
+		if (product) {
+			let cartItem;
+			let find = this.cartItems.find((item) => {
+				if (item.product == product) {
+					return item.product;
+				}
+			});
+			if (find) {
+				find.count++;
+			} else {
+				cartItem = {
+					product,
+					count: 1,
+				};
+				this.cartItems.push(cartItem);
+			}
+			this.onProductUpdate(cartItem);
+		}
+	}
 
-  updateProductCount(productId, amount) {
-    // ваш код
-  }
+	updateProductCount(productId, amount) {
+		let cartItem;
 
-  isEmpty() {
-    // ваш код
-  }
+		this.cartItems.find((cartItem) => {
+			if (cartItem) {
+				if (cartItem.product.id == productId) {
+					cartItem.count += amount;
+					if (cartItem.count === 0) {
+						let index = this.cartItems.indexOf(cartItem);
+						this.cartItems.splice(index, 1);
+						return;
+					}
+				}
+			}
+			return;
+		});
 
-  getTotalCount() {
-    // ваш код
-  }
+		this.onProductUpdate(cartItem);
+		this.getTotalPrice();
+	}
 
-  getTotalPrice() {
-    // ваш код
-  }
+	isEmpty() {
+		if (this.cartItems.length > 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
-  onProductUpdate(cartItem) {
-    // реализуем в следующей задаче
+	getTotalCount() {
+		let count = 0;
+		this.cartItems.find((item) => {
+			count += item.count;
+		});
+		return count;
+	}
 
-    this.cartIcon.update(this);
-  }
+	getTotalPrice() {
+		let price = 0;
+
+		this.cartItems.find((item) => {
+			price += item.product.price * item.count;
+		});
+		return price;
+	}
+
+	onProductUpdate(cartItem) {
+		// реализуем в следующей задаче
+
+		this.cartIcon.update(this);
+	}
 }
-
